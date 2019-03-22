7944edd781fe028cdd4104cd40d65b98f93bcae6
Sabrina <zsbrina20@gmail.com>
2019-03-22 18:36:07 -0500

    fix db

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "3anofjnono",
-  database: "burger_db"
-});
+var connection;
 
-// Make connection.
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '3anofjnono',
+    database: 'burger_db'
+  });
+};
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Chesse burger');
+INSERT INTO burgers (name) VALUES ('Test1');
+INSERT INTO burgers (name, devour) VALUES ('test2', true);
+INSERT INTO burgers (name, devour) VALUES ('test3', true);
+INSERT INTO burgers (name, devour) VALUES ('test4', true);
